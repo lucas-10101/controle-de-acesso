f8af71071790044d03f259368a681b7cd4a7c401
lucas-10101 <lucas-quadros@outlook.com.br>
2024-03-02 13:09:26 -0300

    Properties

---
+++ src/main/java/tms/backend/controledeacesso/MicroservicoDeControleDeAcessoApplication.java
@@ -0,0 +1,13 @@
+package tms.backend.controledeacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicoDeControleDeAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicoDeControleDeAcessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tms/backend/controledeacesso/MicroservicoDeControleDeAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package tms.backend.controledeacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoDeControleDeAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
